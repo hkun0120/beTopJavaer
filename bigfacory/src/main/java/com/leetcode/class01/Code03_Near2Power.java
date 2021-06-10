4f9ba4a3d45b764d657b6b1128ac1bf0ae63a454
hong kun <k@qq.com>
2021-06-10 14:44:00 +0800

    排序和移位

--- bigfacory/src/main/java/com/leetcode/class01/Code03_Near2Power.java
+++ bigfacory/src/main/java/com/leetcode/class01/Code03_Near2Power.java
@@ -10,4 +10,32 @@ package com.leetcode.class01;
  * @create: 2021-06-03 22:39
  */
 public class Code03_Near2Power {
+    public static void main(String[] args) {
+        int i = 2>>3;
+        System.out.println(i);
+        System.out.println(tableSizeFor(120));
+    }
+
+    /**
+     * 填满函数， | 代表或， >>>无符号位右移 ，n和右移之后的数字进行或运算，就逐渐把右边的数字变为了1，
+     * 比如，n=29，011101 右移1位变为001110，或运算变为011111 再移动两位，变为000111，再或变为011111，依此类推
+     * 最后得到011111
+     * 为什么先-1？因为当数字正好是2的n次方时，先减最后+，还是这个数本身
+     * 为什么右移1，2，4，8，16 加起来是31，int类型一共就32位
+     * 为什么最后要判断n<0，如果是个负数的话，2的n次方最低也是1，离负数最近的就是2的0次方1
+     * >> 右移，若负数，则用1来补首位
+     * @param n
+     * @return
+     */
+    public static int tableSizeFor(int n){
+        n --;
+        n |= n>>>1;
+        n |= n>>>2;
+        n |= n>>>4;
+        n |= n>>>8;
+        n |= n>>>16;
+
+        return (n<0)?1:n +1;
+
+    }
 }
--- bigfacory/src/main/java/com/leetcode/class01/Code04_MinSwapStep.java
+++ bigfacory/src/main/java/com/leetcode/class01/Code04_MinSwapStep.java
@@ -2,8 +2,46 @@ package com.leetcode.class01;
 
 /**
  * @description:
+ *  题目：
+ *      一个数组中只有两种字符G和B，
+ *      想让所有的G都放入左侧，所有的B都放入右侧
+ *      但是只能在相邻的字符之间进行交换操作
+ *      返回至少需要交换几次
+ *
  * @author: hk2018
  * @create: 2021-06-03 22:40
  */
 public class Code04_MinSwapStep {
+    /**
+     * 时间复杂度为O(N)最优解
+     * 两个变量即完成排序
+     * 下面的算法是解了这样的题目（G 、B可以左边或者右边，所以代码里需要求G、B两种情况下的最小移动次数）
+     */
+    public static int minStep1(String s){
+        if(s ==null||s.equals("")){
+            return 0;
+        }
+        char[] arr =s.toCharArray();
+        int gi = 0;
+        int step1 =0;
+        for(int i = 0; i<arr.length;i++){
+            if(arr[i] =='G'){
+                step1 += i - (gi++);
+            }
+        }
+
+        int step2 = 0;
+        int bi =0;
+        for(int i= 0 ;i<arr.length;i++){
+            if(arr[i] =='B'){
+                step2 += i - (bi++);
+            }
+        }
+        return Math.min(step1,step2);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minStep1("BBBGGB"));
+    }
 }
